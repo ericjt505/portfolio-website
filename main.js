809262e935c2d2dddc333ec6d7435178d4adf4a9
ericjt505 <weirdspacetobe@gmail.com>
2020-08-17 21:18:43 -0400

    updated

---
+++ main.js
@@ -0,0 +1,8 @@
+var displayImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
+
+/* Looping through images */
+
+var newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+thumbBar.appendChild(newImage);
