ed047c65c1361ac42ec688b0f6cd583b66407bc5
ericjt505 <weirdspacetobe@gmail.com>
2020-08-17 21:58:33 -0400

    update

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 var displayImage = document.querySelector('.displayed-img');
 var thumbBar = document.querySelector('.thumb-bar');
 
+var btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
+
 /* Looping through images */
 
-var newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for(let i = 1; i <= 5; i++) {
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', xxx);
+  thumbBar.appendChild(newImage);
+  newImage.onclick = function(e) {
+    displayedImage.src = e.target.src;
+  }
+}
+
+btn.onclick = function() {
+  var btnClass = btn.getAttribute('class');
+  if(btnClass === 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Ligthen';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+}
