83090986b183cc2279081935008f64a6155b67e7
ericjt505 <weirdspacetobe@gmail.com>
2020-08-20 14:47:27 -0400

    update 8/20/20 @ 2:47pm

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
-function changeImage(event) {
-  event = event || window.event;
+var displayedImage = document.querySelector(".displayed-img");
+var thumbBar = document.querySelector(".thumb-bar");
 
-  var targetElement = event.target || event.srcElement;
+/* Looping through images */
 
-  if(targetElement.tagName == 'IMG') {
-    document.getElementById("displayed-img").src = targetElement.getAttribute("src");
+for(let i = 1; i <= 5; i++) {
+  var newImage = document.createElement("img");
+  newImage.setAttribute("src", "images/pic" + i + ".jpg");
+  thumbBar.appendChild(newImage);
+  newImage.onclick = function(event) {
+    event = event || window.event;
+
+    var targetElement = event.target || event.srcElement;
+
+    if(targetElement.tagName == "IMG") {
+      document.getElementById("displayed-img").src = targetElement.getAttribute("src");
+    }
+  }
+}
+
+var btn = document.querySelector("button");
+var overlay = document.querySelector(".overlay");
+
+btn.onclick = function() {
+  var btnClass = btn.getAttribute("class");
+  if(btnClass === "dark") {
+  btn.setAttribute("class", "light");
+  btn.textContent = "Ligthen";
+  overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+  } else {
+    btn.setAttribute("class", "dark");
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgba(0,0,0,0)";
   }
 }
